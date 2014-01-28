86f0c7a8f70e3757687b27add512c92d51814b0d
Helio Medeiros <helio.cabralmedeiros@gmail.com>
2014-01-27 23:01:27 -0200

    Adding shared repository creation

--- svn-to-git-migrator.sh
+++ svn-to-git-migrator.sh
@@ -26,3 +26,15 @@ echo "Transforming from SVN $url/$PROJECT_OWNER/$PROJECT_NAME project to GIT"
 
 echo "Getting from SVN $url/$PROJECT_OWNER/$PROJECT_NAME"
 git svn clone --username $USER $url/$PROJECT_OWNER/ $PROJECT_NAME
+
+echo "Do you want to create an shared repository from $PROJECT_NAME ?"
+read REMOTE_GIT_REPO
+
+if [ "$REMOTE_GIT_REPO" == "" ] || [ "$REMOTE_GIT_REPO" == "NO" ]; then
+        echo -e "\e[31mOk that's your choice."
+        echo -e "\e[39m"
+        exit;
+fi
+
+echo "Creating the new shared repository $PROJECT_NAME.git"
+git clone --bare --shared $PROJECT_NAME
